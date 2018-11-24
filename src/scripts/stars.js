a3f5e7b62bff25c562aa329ca6ca0adcbe3c7005
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-11-24 23:55:54 +0100

    Find working solution for adding class to proper stars

--- src/scripts/stars.js
+++ src/scripts/stars.js
@@ -1,27 +1,23 @@
 const section = document.querySelector(".section--products")
 const starBoxes = document.querySelectorAll(".stars");
+const starClasses = ['s1', 's2', 's3', 's4', 's5']
 const starBoxesId = [];
 
 for (i=0; i<starBoxes.length; i++) {
-    starBoxesId.push(`#rate${i+1}`);
+    starBoxesId.push(`rate${i+1}`);
 }
 
-section.addEventListener("mouseover", selectStars(e))
+section.addEventListener("mouseover", function(e) {
+    for (i=0; i<starBoxesId.length; i++) {
+        if (event.target.classList.contains('s3')) {
+            let num = 3;
+            selectStars();
+        }
+        else {}
+    }   
+});
 
-console.log(section);
-
-function selectStars(e) {
-    console.log(e.target);
+function selectStars() {
+    event.target.parentElement.children[1].classList.add("active");
+    event.target.parentElement.children[2].classList.add("active");
 }
-
-
-
-/*for (i=0; i<starBoxesId.length; i++) {
-    const starBox = document.querySelector("#"+starBoxesId[i]);
-    starBox.addEventListener("mouseover", function() {
-
-        console.log([starBox])
-    })
-}*/
-
-  
