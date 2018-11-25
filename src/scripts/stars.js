fa74ad19ddbcb9c3e790d56c91cf55992e421f3b
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-11-25 11:47:28 +0100

    Finish first part - marking stars when hover

--- src/scripts/stars.js
+++ src/scripts/stars.js
@@ -1,23 +1,40 @@
 const section = document.querySelector(".section--products")
 const starBoxes = document.querySelectorAll(".stars");
-const starClasses = ['s1', 's2', 's3', 's4', 's5']
 const starBoxesId = [];
+const stars = document.querySelectorAll(".star");
 
 for (i=0; i<starBoxes.length; i++) {
     starBoxesId.push(`rate${i+1}`);
 }
 
 section.addEventListener("mouseover", function(e) {
-    for (i=0; i<starBoxesId.length; i++) {
-        if (event.target.classList.contains('s3')) {
-            let num = 3;
-            selectStars();
+    //for (i=0; i<starBoxesId.length; i++) {
+        if (event.target.classList.contains('s1')) {
+            event.target.parentElement.children[0].classList.add("active");
         }
-        else {}
+        else if (event.target.classList.contains('s2')) {
+            event.target.parentElement.children[0].classList.add("active");
+            event.target.parentElement.children[1].classList.add("active");
+        }
+        else if (event.target.classList.contains('s3')) {
+            event.target.parentElement.children[0].classList.add("active");
+            event.target.parentElement.children[1].classList.add("active");
+            event.target.parentElement.children[2].classList.add("active");
+        }
+        else if (event.target.classList.contains('s4')) {
+            event.target.parentElement.children[0].classList.add("active");
+            event.target.parentElement.children[1].classList.add("active");
+            event.target.parentElement.children[2].classList.add("active");
+            event.target.parentElement.children[3].classList.add("active");
+        }
+        else if (event.target.classList.contains('s5')) {
+            event.target.parentElement.children[0].classList.add("active");
+            event.target.parentElement.children[1].classList.add("active");
+            event.target.parentElement.children[2].classList.add("active");
+            event.target.parentElement.children[3].classList.add("active");
+            event.target.parentElement.children[4].classList.add("active");
+        }
+        else {};
     }   
-});
-
-function selectStars() {
-    event.target.parentElement.children[1].classList.add("active");
-    event.target.parentElement.children[2].classList.add("active");
-}
+//}
+);
