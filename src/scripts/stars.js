589eb789863aab9219e298b815059ee7861983db
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-11-26 15:17:27 +0100

    Fix showing avg value when not hover even if marked

--- src/scripts/stars.js
+++ src/scripts/stars.js
@@ -1,44 +1,48 @@
 const section = document.querySelector(".section--products")
 const starBoxes = document.querySelectorAll(".stars");
-const starBoxesId = [];
-const stars = document.querySelectorAll(".star");
-
-for (i=0; i<starBoxes.length; i++) {
-    starBoxesId.push(`rate${i+1}`);
-}
-
 
 section.addEventListener("mouseover", function(e) {
     if (event.target.classList.contains('s1')) {
-        desactiveStars();
         selectStars(0);
     }
     else if (event.target.classList.contains('s2')) {
-        desactiveStars();
         selectStars(1);
     }
     else if (event.target.classList.contains('s3')) {
-        desactiveStars();
         selectStars(2);
     }
     else if (event.target.classList.contains('s4')) {
-        desactiveStars();
         selectStars(3);
     }
     else if (event.target.classList.contains('s5')) {
-        desactiveStars();
         selectStars(4);
     }
     else if (event.target.classList.contains('stars')) {
-        console.log('out')
         deselectStars();
     }   
 });
 
-
+section.addEventListener("click", function(e) {
+    if (event.target.classList.contains('s1')) {
+        markStars(0);
+    }
+    else if (event.target.classList.contains('s2')) {
+        markStars(1);
+    }
+    else if (event.target.classList.contains('s3')) {
+        markStars(2);
+    }
+    else if (event.target.classList.contains('s4')) {
+        markStars(3);
+    }
+    else if (event.target.classList.contains('s5')) {
+        markStars(4);
+    } 
+});
 
 // temporary selecting stars
 function selectStars(num) {
+    desactiveStars();
     for (i=0; i<=num; i++) {
         event.relatedTarget.children[i].classList.add("active")
     }
@@ -46,8 +50,11 @@ function selectStars(num) {
 
 // deselecting stars
 function deselectStars() {
-    for (i=0; i<=1; i++) {
-        event.target.children[i].classList.add("full");
+    if (event.target.children[0].classList.contains("permanent")){}
+    else {
+        for (i=0; i<=1; i++) {
+            event.target.children[i].classList.add("full");
+        }
     }
     for (i=0; i<=4; i++) {
         event.target.children[i].classList.remove("active");
@@ -57,5 +64,19 @@ function deselectStars() {
 function desactiveStars() {
     for (i=0; i<=4; i++) {
         event.target.parentElement.children[i].classList.remove("full");
+    }
+}
+// permanent selecting stars
+function markStars(num) {
+    desactiveStars();
+    unmarkStars();
+    for (i=0; i<=num; i++) {
+        event.target.parentElement.children[i].classList.add("permanent");
+    }
+}
+// unmarking stars (for changing vote)
+function unmarkStars(num) {
+    for (i=0; i<=4; i++) {
+        event.target.parentElement.children[i].classList.remove("permanent");
     }
 }
