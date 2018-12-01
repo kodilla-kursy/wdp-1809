a6c0b3de15f4fc1ae7548a321179fb8092a013ae
Grzes Olejniczak <olejniczak.grzes@gmail.com>
2018-12-01 13:08:07 +0100

    Add tiny slider to furniture section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,3 +10,31 @@
         showBtn.classList.toggle('active');
     });
 })();  
+
+//TINY-SLIDER - NEW FURNITURE
+
+(function(){
+
+    var productsSlider = tns({
+        container: '.products-slider',
+        items: 1,
+        slideBy: 1,
+        mouseDrag: true,
+        controls: false,
+        navContainer: '.products-carousel-indicators',
+        responsive: {
+            768: {
+            items: 2,
+            slideBy: 2,
+            },
+            992: {
+            items: 3,
+            slideBy: 3,
+            },
+            1200: {
+            items: 4,
+            slideBy: 4,
+            }
+          }
+    });
+})();  
