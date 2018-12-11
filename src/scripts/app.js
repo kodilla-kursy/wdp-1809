506a01d2c2f4a5b102c15e614083a67f86b36a0c
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-12-11 17:22:57 +0100

    Add swipe and remove hover on tablets/mobiles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -133,3 +133,15 @@ function unsetStarsClass(limit, elem, className) {
       });
 
 })();  
+
+
+// Mobile swipe for features carousel - task WDP181103-23
+$(".carousel").swipe({
+
+    swipe: function(event, direction, distance, duration, fingerCount, fingerData) { 
+      if (direction == 'left') $(this).carousel('next');
+      if (direction == 'right') $(this).carousel('prev');
+    },
+    allowPageScroll:"vertical"
+  
+  });
