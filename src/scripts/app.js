2f4ea819edeb3642987d4d78b907f0a46b47a973
Pawel Butra <pawel.butra@onet.pl>
2019-01-31 13:57:45 +0100

    Add font resize in app.js for cart counter

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,24 @@
 console.log('hello');
+
+/* Resising cart counter font  if  text overflows div element */
+
+function isOverflown (element) {
+  return element.scrollWidth > element.clientWidth;
+}
+
+let cartCounter = document.querySelector('.cart-counter');
+cartCounter.style.fontSize = '14px';
+
+let cartFontSize = parseInt(cartCounter.style.fontSize);
+console.log(cartFontSize);
+for (let i = cartFontSize; i >= 0; i--) {
+  let counterOverflown = isOverflown(cartCounter);
+  if (!counterOverflown) {
+    console.log('no overflow');
+  } else {
+    cartFontSize--;
+    console.log(cartFontSize);
+    cartCounter.style.fontSize = cartFontSize + 'px';
+    cartCounter.style.paddingLeft = '2px';
+  }
+}
