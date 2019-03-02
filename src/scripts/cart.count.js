4829440c5027356d910641814166f47d7270e3c2
Marcin Woźniak <wozniak.marcindneu@gmail.com>
2019-03-02 18:50:27 +0100

    Remove commented code

--- src/scripts/cart.count.js
+++ src/scripts/cart.count.js
@@ -2,29 +2,6 @@
 
 const shoppingCartCounter = document.querySelector('.cart-counter');
 
-// Opcja z ingerencją w style CSS, określająca max. wskaźnik
 if (shoppingCartCounter.innerText > 99999) {
   shoppingCartCounter.innerHTML = 99999;
 }
-
-/* Opcja w pełni JS, bez ingerencji w style CSS
-function adjustCartCounterDisplay () {
-  let shoppingCartItemsCount = shoppingCartCounter.innerText.length;
-
-  if (shoppingCartItemsCount < 4) {
-    shoppingCartCounter.style.fontSize = '13px';
-  } else if (shoppingCartItemsCount > 3 && shoppingCartItemsCount < 5) {
-    shoppingCartCounter.style.fontSize = '9px';
-    shoppingCartCounter.style.paddingTop = '3px';
-  } else if (shoppingCartItemsCount === 5) {
-    shoppingCartCounter.style.fontSize = '8px';
-    shoppingCartCounter.style.paddingTop = '4px';
-  } else if (shoppingCartItemsCount > 5) {
-    shoppingCartCounter.innerHTML = '...';
-    shoppingCartCounter.style.paddingBottom = '3px';
-    shoppingCartCounter.style.color = 'rgb(187, 0, 0)';
-  }
-}
-
-shoppingCartCounter.addEventListener('change', adjustCartCounterDisplay());
-*/
