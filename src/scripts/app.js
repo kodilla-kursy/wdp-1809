decb5867deaf761c7ea53a012039b7fd36fa969b
Bogus Franczak <bfranczak@gmail.com>
2019-03-08 18:57:18 +0100

    Write initial code for carousel

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
-console.log('hello');
+import { tns } from 'tiny-slider';
+
+const myOptions = {
+  container: '.carousel-furniture',
+  items: 1,
+  autoplay: false
+};
+
+const slider = tns(myOptions);
+
+console.log(slider.isOn);
