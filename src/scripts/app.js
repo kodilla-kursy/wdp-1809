a4158a56f9af6c3759cc79c381b5bceb30b179f5
Bogus Franczak <bfranczak@gmail.com>
2019-03-10 17:05:40 +0100

    Adjust responsive options, add function to set initiate number of nav dots

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,34 @@
-import { tns } from 'tiny-slider';
+const createCarousel = (carouselContainer, navContainer) => {
+  const carouselOptions = {
+    container: carouselContainer,
+    mode: 'gallery',
+    autoplay: false,
+    controls: false,
+    items: 1,
+    navContainer: navContainer,
+    responsive: {
+      576: {
+        items: 2
+      },
+      992: {
+        items: 3
+      }
+    },
+    navAsThumbnails: false
+  };
 
-const myOptions = {
-  container: '.carousel-furniture',
-  items: 1,
-  autoplay: false
+  // eslint-disable-next-line no-undef
+  const carousel = tns(carouselOptions);
+  const info = carousel.getInfo();
+
+  hideInactiveNavs(info.navItems, info.pages);
 };
 
-const slider = tns(myOptions);
+const hideInactiveNavs = (navs, pageCount) => {
+  for (let i = 0; i < navs.length; i++) {
+    const element = navs[i];
+    if (i >= pageCount) element.style = 'display: none';
+  }
+};
 
-console.log(slider.isOn);
+createCarousel('.carousel-furniture', '.furniture-carousel-nav');
