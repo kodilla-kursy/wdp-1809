c5f1bc5bce9715c792e2dc367d2bec91a67dd897
Ola Podsiadlik <alecs@op.pl>
2019-03-11 21:53:17 +0100

    Try init tiny-slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,27 @@
-console.log('hello');
+const createCarousel = (carouselContainer, navContainer) => {
+  const carouselOptions = {
+    container: carouselContainer,
+    mode: 'gallery',
+    autoplay: false,
+    controls: false,
+    items: 1,
+    navContainer: navContainer,
+
+    navAsThumbnails: false
+  };
+
+  // eslint-disable-next-line no-undef
+  const carousel = tns(carouselOptions);
+  const info = carousel.getInfo();
+
+  hideInactiveNavs(info.navItems, info.pages);
+};
+
+const hideInactiveNavs = (navs, pageCount) => {
+  for (let i = 0; i < navs.length; i++) {
+    const element = navs[i];
+    if (i >= pageCount) element.style = 'display: none';
+  }
+};
+
+createCarousel('.carousel-hot-deals', '.hot-deals-carousel-nav');
