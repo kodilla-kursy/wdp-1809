0c93324f734575e175f66291c5411c7b1b60fd04
Roman Jędras <romanjedras@o2.pl>
2019-05-22 17:14:44 +0200

    Add function to features

--- src/scripts/features.js
+++ src/scripts/features.js
@@ -4,18 +4,17 @@ console.log(features);
 features[0].classList.add('active');
 
 features.forEach(function (item) {
-  let link = document.createElement('a');
-  console.log(link);
+  let link = createElement('a');
   link.href = '#';
+
   link.insertAdjacentElement('afterbegin', item.childNodes[1]);
   link.appendChild(item.childNodes[2]);
-  item.appendChild(link);
+  appendChild(item, link);
   item.addEventListener('mouseover', function (event) {
     event.preventDefault();
     features[0].classList.remove('active');
     this.classList.add('active');
   });
-
   item.addEventListener('mouseout', function (event) {
     event.preventDefault();
     for (let i = 1; i < features.length - 1; i++) {
@@ -24,3 +23,11 @@ features.forEach(function (item) {
     features[0].classList.add('active');
   });
 });
+
+function createElement (itemName) {
+  return document.createElement(itemName);
+}
+
+function appendChild (parentNode, childNode) {
+  return parentNode.appendChild(childNode);
+}
