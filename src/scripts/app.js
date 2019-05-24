9ea88c7b9f18eaf17c251c069a35a730e7c82311
Pawel Sleziona <pawel.sleziona@gmail.com>
2019-05-25 01:13:46 +0200

    WIP add onclick action

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,17 +2,13 @@ var ratingContainers = document.querySelectorAll('.stars');
 
 ratingContainers.forEach(function (item) {
   for (var i = 0; i < item.children.length; i++) {
-    item.children[i].addEventListener('mouseenter', function (e) {
-      e.preventDefault();
-      changeSiblings(e.target);
-    });
-    item.children[i].addEventListener('mouseleave', function (e) {
-      e.preventDefault();
-      clearSiblins(e.target);
-    });
+    item.children[i].addEventListener('mouseenter', eventMouseIn);
+    item.children[i].addEventListener('mouseleave', eventMouseOut);
     item.children[i].addEventListener('click', function (e) {
       e.preventDefault();
       ratingOnClick(e.target);
+      removeNextListeners(this);
+      removePrevListeners(this);
     });
   }
 });
@@ -33,17 +29,38 @@ function clearSiblins (element) {
     element.style.color = '';
     element.style.fontWeight = 400;
   }
-  if (element.classList.contains('active')) {
-    element.style.fontWeight = 900;
-  }
   if (element.previousElementSibling) {
     clearSiblins(element.previousElementSibling);
   }
 }
 
 function ratingOnClick (element) {
-  element.classList.toggle('active');
+  element.style.color = '#d58e32';
   if (element.previousElementSibling) {
     ratingOnClick(element.previousElementSibling);
   }
+}
+
+function removePrevListeners (element) {
+  element.removeEventListener('mouseleave', eventMouseOut);
+  element.removeEventListener('mouseenter', eventMouseIn);
+  if (element.previousElementSibling) {
+    removePrevListeners(element.previousElementSibling);
+  }
+}
+
+function removeNextListeners (element) {
+  element.removeEventListener('mouseleave', eventMouseOut);
+  element.removeEventListener('mouseenter', eventMouseIn);
+  if (element.nextElementSibling) {
+    removeNextListeners(element.nextElementSibling);
+  }
+}
+
+function eventMouseOut () {
+  clearSiblins(this);
+}
+
+function eventMouseIn () {
+  changeSiblings(this);
 }
