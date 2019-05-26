0a45806a0a4f02cdd8b8dfff865553e5644e16c4
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-05-26 18:04:32 +0200

    Add flickity to blog section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,2 @@
+import './blogSlider';
 console.log('hello');
---
+++ src/scripts/blogSlider.js
@@ -0,0 +1,6 @@
+import Flickity from 'flickity';
+
+const blogSlider = new Flickity('.blog-slider', {
+  wrapAround: true
+});
+export default blogSlider;
