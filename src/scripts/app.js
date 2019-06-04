09d3f335f26a837caf7564e4dcb54bb927c5e234
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-06-04 22:42:39 +0200

    Add flickity function, add flickity css to head

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,5 @@
-import './blogSlider';
+import flickitySlider from './sliders';
 console.log('hello');
+
+flickitySlider('.blog-slider', { wrapAround: true });
+// for new flickity use function flickitySlider( carousel-element, { flickity options })
--- src/scripts/blogSlider.js
+++
@@ -1,6 +0,0 @@
-import Flickity from 'flickity';
-
-const blogSlider = new Flickity('.blog-slider', {
-  wrapAround: true
-});
-export default blogSlider;
