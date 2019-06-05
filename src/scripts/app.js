4daed9ec07761b1182759e563d958ce8d292cc3b
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-06-06 00:14:27 +0200

    Update code to use slilder as sugested in task 20

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,5 @@
-import './productSlider';
-
+import flickitySlider from './sliders';
 console.log('hello');
+
+flickitySlider('.products-slider', { wrapAround: true });
+// for new flickity use function flickitySlider( carousel-element, { flickity options })
--- src/scripts/productSlider.js
+++
@@ -1,7 +0,0 @@
-import Flickity from 'flickity';
-
-const productSlider = new Flickity('.products-carousel', {
-  wrapAround: true
-});
-
-export default productSlider;
