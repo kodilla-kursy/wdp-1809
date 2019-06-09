860700e49a65376ec1c08d8fa02576cf3010e62a
bio8oid <8bollod8@mail.co.uk>
2019-06-09 18:26:50 +0200

    Change filckity init to run from sliders.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,12 @@
-console.log('hello');
+import flickitySlider from './sliders';
+
+flickitySlider('.brands-slider', {
+  wrapAround: true,
+  pageDots: false,
+  groupCells: '100%',
+  cellAlign: 'center',
+  contain: true
+});
+
+flickitySlider('.blog-slider', { wrapAround: true });
+// for new flickity use function flickitySlider( carousel-element, { flickity options })
