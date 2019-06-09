dd3a5f5b0197015a99493b1d59ff05d01b6631fc
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-06-09 20:46:18 +0200

    Add flickity to tabs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,36 @@
 import flickitySlider from './sliders';
-console.log('hello');
+import $ from 'jquery';
+import './bootstrap.min.js';
 
-flickitySlider('.products-slider', { wrapAround: true });
-// for new flickity use function flickitySlider( carousel-element, { flickity options })
+// Product section sliders in tabs
+
+const bedSlider = flickitySlider('#bed', { wrapAround: true });
+const chairSlider = flickitySlider('#chair', { wrapAround: true });
+const sofaSlider = flickitySlider('#sofa', { wrapAround: true });
+const tableSlider = flickitySlider('#table', { wrapAround: true });
+const diningSlider = flickitySlider('#dining', { wrapAround: true });
+
+$(document).ready(function () {
+  $('.nav-tabs a').click(function () {
+    $(this).tab('show');
+  });
+  $('.nav-tabs a').on('shown.bs.tab', function (e) {
+    const slider = e.target.dataset.slider;
+    switch (slider) {
+      case 'bedSlider':
+        bedSlider.resize();
+        break;
+      case 'chairSlider':
+        chairSlider.resize();
+        break;
+      case 'sofaSlider':
+        sofaSlider.resize();
+        break;
+      case 'tableSlider':
+        tableSlider.resize();
+        break;
+      case 'diningSlider':
+        diningSlider.resize();
+    }
+  });
+});
